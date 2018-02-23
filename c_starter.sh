9e37c4727901eae673dd60b6b0d83ed043c0482f
Legolas202 <court.remy2@gmail.com>
2018-02-23 16:53:03 +0100

    Make the wallpaper appear when you use the launcher

--- c_starter.sh
+++ c_starter.sh
@@ -4,4 +4,5 @@ conky -c /home/legolas20/conky/.conkyrc &
 conky -c /home/legolas20/conky/.conkyrc4 &
 conky -c /home/legolas20/conky/.clock/.conkyrc6 &
 conky -c /home/legolas20/conky/.circle/circle &
+gsettings set org.gnome.desktop.background picture-uri "file:///home/legolas20/conky/Wallpaper.jpg"
 exit
